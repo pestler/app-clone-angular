12310fae4efffa933a6f8744a2fdd3a5e9eac3b1
Aleh Kuis <pestleralex@gmail.com>
2025-09-09 13:36:30 +0300

    feat: formatting code

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,13 +1,8 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard-scss',
-    'stylelint-prettier/recommended',
-  ],
+  extends: ['stylelint-config-standard-scss', 'stylelint-prettier/recommended'],
   rules: {
-    "at-rule-no-unknown": null,
-    "no-empty-source": true,
-    "length-zero-no-unit": true,
+    'at-rule-no-unknown': null,
+    'no-empty-source': true,
+    'length-zero-no-unit': true,
   },
 };
-
-
