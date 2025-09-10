05731a16a4e9753d697852111cdab570510e5efc
Aleh Kuis <pestleralex@gmail.com>
2025-09-10 12:31:03 +0300

    chore: Integrate Firebase and AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,23 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideRouter } from '@angular/router';
-
 import { environment } from '../environments/environment';
-
 import { routes } from './app.routes';
+export class GlobalErrorHandler implements ErrorHandler {
+  handleError(error: unknown): void {
+    console.error('Global Error:', error);
+  }
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    { provide: ErrorHandler, useClass: GlobalErrorHandler },
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
