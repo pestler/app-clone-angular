6881729764bfe66c9d00d6a0d551f683f38cdb0f
Aleh Kuis <pestleralex@gmail.com>
2025-09-10 16:11:07 +0300

    feat(login): create login page UI

---
+++ src/app/pages/login.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AuthService } from '../core/services/auth';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  private authService = inject(AuthService);
+
+  login(): void {
+    this.authService.signInWithGitHub();
+  }
+}
