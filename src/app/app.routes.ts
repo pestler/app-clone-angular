7886b81112067dcfe1dec980a93d9c85ed4e88df
Ekaterina Gorbacheva <108616376+Kavume@users.noreply.github.com>
2025-09-10 16:25:04 +0200

    feat: Create Core Layout Structure #10 (#22)
    * feat: create layout component
    
    * fix: test
    
    * chore: configure schematics defaults
    
    * feat: del component
    
    ---------
    
    Co-authored-by: Aleh Kuis <pestleralex@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout.component';
 import { LoginComponent } from './pages/login.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+  },
+
   { path: 'login', component: LoginComponent },
   {
     path: '',
