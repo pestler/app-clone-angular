bae85225fd188116648bb4ff58199e3c55a728c5
Kavume <e.gorbacheva404@gmail.com>
2025-09-11 02:19:19 +0200

    fix: test

--- src/app/layout/layout.component.spec.ts
+++ src/app/layout/layout.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { Layout } from './layout.component';
 
 describe('Layout', () => {
@@ -9,6 +10,7 @@ describe('Layout', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Layout],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Layout);
