40d9860381767ede976cbb411b0bc92095de1cba
Kavume <e.gorbacheva404@gmail.com>
2025-09-13 01:30:25 +0200

    feat: create side navigation component

---
+++ src/app/pages/dashboard/side-nav/side-nav.data.ts
@@ -0,0 +1,22 @@
+export interface SideNavProps {
+  path: string;
+  label: string;
+  icon: string;
+  color: string;
+}
+
+/**
+ * NOTE: These icons are temporary placeholders. They will be replaced later
+ */
+
+export const sideNavItems: SideNavProps[] = [
+  { path: '', label: 'Dashboard', icon: 'dashboard', color: 'var(--icon-blue)' },
+  { path: '', label: 'Score', icon: 'home', color: 'var(--icon-orange)' },
+  { path: '', label: 'Schedule', icon: 'calendar_today', color: 'var(--icon-pink)' },
+  { path: '', label: 'Cross-Check: Submit', icon: 'terminal', color: 'var(--icon-blue)' },
+  { path: '', label: 'Cross-Check: Review', icon: 'check_circle', color: 'var(--icon-red)' },
+  { path: '', label: 'Interviews', icon: 'mic', color: 'var(--icon-blue)' },
+  { path: '', label: 'Auto-Test', icon: 'play_circle', color: 'var(--icon-violet)' },
+  { path: '', label: 'Team Distributions', icon: 'group', color: 'var(--icon-violet)' },
+  { path: '', label: 'Course Statistics', icon: 'table_chart', color: 'var(--icon-blue)' },
+];
