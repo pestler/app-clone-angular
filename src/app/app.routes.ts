c22f58291f7ae33ce61295d0f67b3969bc1f529f
Aleh Kuis <pestleralex@gmail.com>
2025-09-12 12:22:43 +0300

    feat: add dashboard component (#33)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,33 @@
 import { Routes } from '@angular/router';
+
 import { Layout } from './layout/layout.component';
-import { LoginComponent } from './pages/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: Layout,
+    path: 'login',
+    component: LoginComponent,
   },
 
-  { path: 'login', component: LoginComponent },
   {
     path: '',
-    redirectTo: '/login',
-    pathMatch: 'full',
+    component: Layout,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login',
   },
 ];
