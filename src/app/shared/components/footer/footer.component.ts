354f754b48d0c788fc4a914c669f05852aa007a6
Aleh Kuis <pestleralex@gmail.com>
2025-09-12 11:11:00 +0300

    feat: add link for social icons

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,10 @@
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FOOTER_SECTIONS, FooterSection, SOCIAL_LINKS } from './footer-links';
 
 @Component({
   selector: 'app-footer',
-  imports: [DatePipe],
+  imports: [DatePipe, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
