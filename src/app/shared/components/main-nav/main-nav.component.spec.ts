ce8a0e5c1d8d31ac65ac19dcb8f1bd8879eab293
Kavume <e.gorbacheva404@gmail.com>
2025-09-13 17:00:27 +0200

    fix: test

--- src/app/shared/components/main-nav/main-nav.component.spec.ts
+++ src/app/shared/components/main-nav/main-nav.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { MainNavComponent } from './main-nav.component';
 
@@ -9,6 +10,7 @@ describe('MainNavComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MainNavComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainNavComponent);
