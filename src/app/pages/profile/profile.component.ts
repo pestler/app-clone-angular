23e8f2cec3e832fcda6197fd51bb479ac8b7bec8
Kavume <e.gorbacheva404@gmail.com>
2025-09-21 23:33:31 +0200

    feat: add contacts card with form logic

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AboutCardComponent } from './cards/about-card/about-card.component';
+import { ContactsCardComponent } from './cards/contacts-card/contacts-card.component';
 import { LanguagesCardComponent } from './cards/languages-card/languages-card.component';
 import { UserCardComponent } from './cards/user-card/user-card.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [LanguagesCardComponent, AboutCardComponent, UserCardComponent],
+  imports: [LanguagesCardComponent, AboutCardComponent, UserCardComponent, ContactsCardComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
