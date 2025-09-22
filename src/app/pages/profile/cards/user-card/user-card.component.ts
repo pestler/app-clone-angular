3520b1498fcb4e077767ef81d77aacdccb687dfb
Kavume <e.gorbacheva404@gmail.com>
2025-09-22 21:22:46 +0200

    feat: add user card dialog component

--- src/app/pages/profile/cards/user-card/user-card.component.ts
+++ src/app/pages/profile/cards/user-card/user-card.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { studentMockInfo } from '../../../../core/mocks/student.mock';
+import { UserCardDialogComponent } from './user-card-dialog/user-card-dialog.component';
 
 @Component({
   selector: 'app-user-card',
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
 })
 export class UserCardComponent {
   user = studentMockInfo;
+  readonly dialog = inject(MatDialog);
+
   openDialog() {
-    console.log('h');
+    const dialogRef = this.dialog.open(UserCardDialogComponent, {
+      width: '500px',
+      data: {
+        name: this.user.name,
+        location: this.user.location,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.user = {
+          ...this.user,
+          name: result.nameCtrl,
+          location: result.locationCtrl,
+        };
+      }
+    });
   }
 }
