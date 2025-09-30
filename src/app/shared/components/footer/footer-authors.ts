fec3730b6f1c8e2f39f6ed08db0ae7c2b610707b
Aleh Kuis <pestleralex@gmail.com>
2025-09-30 10:11:19 +0300

    feat: add avatars

--- src/app/shared/components/footer/footer-authors.ts
+++ src/app/shared/components/footer/footer-authors.ts
@@ -3,6 +3,8 @@ export interface FooterAuthor {
   githubUrl: string;
   iconPath: string;
   role: string;
+  badgeUrl: string;
+  avatarUrl: string;
 }
 
 export const FOOTER_AUTHORS: FooterAuthor[] = [
@@ -11,17 +13,23 @@ export const FOOTER_AUTHORS: FooterAuthor[] = [
     githubUrl: 'https://github.com/pestler',
     iconPath: 'assets/icons/github.svg',
     role: 'Team Lead',
+    badgeUrl: 'https://img.shields.io/github/followers/pestler?style=social',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/28656604?v=4',
   },
   {
     name: 'Aliaksei Tokarau',
     githubUrl: 'https://github.com/aliakseitokarev',
     iconPath: 'assets/icons/github.svg',
     role: 'Developer',
+    badgeUrl: 'https://img.shields.io/github/followers/aliakseitokarev?style=social',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/37328596?v=4',
   },
   {
     name: 'EKATERINA GORBACHEVA',
     githubUrl: 'https://github.com/Kavume',
     iconPath: 'assets/icons/github.svg',
     role: 'Developer',
+    badgeUrl: 'https://img.shields.io/github/followers/Kavume?style=social',
+    avatarUrl: 'https://avatars.githubusercontent.com/u/108616376?v=4',
   },
 ];
