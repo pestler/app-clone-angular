9b405a03e9f0f8a7dcbb81ee3e1d317202eeee15
Aleh Kuis <pestleralex@gmail.com>
2025-10-02 17:49:34 +0300

    feat: change test

--- src/app/pages/notifications/notifications.component.spec.ts
+++ src/app/pages/notifications/notifications.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { NotificationsDataService } from '../../core/services/notifications-data.service';
 import { NotificationsComponent } from './notifications.component';
 
 describe('NotificationsComponent', () => {
   let component: NotificationsComponent;
   let fixture: ComponentFixture<NotificationsComponent>;
+  let mockNotificationsDataService: jasmine.SpyObj<NotificationsDataService>;
 
   beforeEach(async () => {
+    mockNotificationsDataService = jasmine.createSpyObj('NotificationsDataService', [
+      'unsubscribe',
+    ]);
+
+    Object.defineProperty(mockNotificationsDataService, 'notifications$', {
+      value: of([]),
+      writable: true,
+    });
+
     await TestBed.configureTestingModule({
       imports: [NotificationsComponent],
+      providers: [{ provide: NotificationsDataService, useValue: mockNotificationsDataService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotificationsComponent);
